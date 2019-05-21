887f1b3aa1d49f287747539077607158b9c2c1f3
bobnearents <bobnearents@yahoo.com>
2019-05-21 11:38:40 -0700

    deployed cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(cors({
-  origin: "http://localhost:3000/"
+  origin: "https://notefuls.bobnearents.now.sh"
 }));
 app.use(helmet());
 app.use('/api/notes', notesRouter);
