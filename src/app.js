87b4952f5f65f082a7d26f0a712fce96517ec812
bobnearents <bobnearents@yahoo.com>
2019-05-21 11:17:01 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,9 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 app.use(helmet());
 app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter); 
